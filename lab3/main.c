c789588b061271f932cd204b5ddec5b942653c2f
Aleksandr Kolesavin <a.v.kolesavin@gmail.com>
2023-03-26 16:40:53 +0300

    add base for lab 3

---
+++ lab3/main.c
@@ -0,0 +1,17 @@
+#define DATA_TYPE char *
+
+#include <stdio.h>
+#include "table/table.h"
+
+int main(int argc, char *argv[])
+{
+    // printf("%s\n", TEST);
+    return 0;
+}
+
+/*
+Table *table = newTable(adapter_structure);
+table.init();
+table.print();
+table.close();
+*/
