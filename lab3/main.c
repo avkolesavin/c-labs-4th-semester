d327b078b46d1c56c8ec2e51ec27abd1cb42b48d
Aleksandr Kolesavin <a.v.kolesavin@gmail.com>
2023-06-28 13:27:29 +0300

    format main.c in lab3

--- lab3/main.c
+++ lab3/main.c
@@ -21,32 +21,26 @@ int main()
     if (fileTable == NULL)
         return 1;
 
-    // printf("size: %u, fullness; %u, step: %d\n", fileTable->size, fileTable->fullness, fileTable->step);
     fileTable->print(fileTable);
-    // fileTable->insert(fileTable, 2, 2, "Hello Table\0\0");
+    fileTable->insert(fileTable, 2, 2, "Hello Table\0\0");
     fileTable->delete (fileTable, 2, 2);
-    // fileTable->insert(fileTable, 6, 6, "Qwerty");
-    // fileTable->insert(fileTable, 9, 9, "Some data");
-    // fileTable->update(fileTable, 9, 9, "Test data");
+    fileTable->insert(fileTable, 6, 6, "Qwerty");
+    fileTable->insert(fileTable, 9, 9, "Some data");
+    fileTable->update(fileTable, 9, 9, "Test data");
     fileTable->print(fileTable);
 
-    // memTable->insert(memTable, 1, 2, "Hello Table");
-    // memTable->insert(memTable, 1, 4, "123");
-    // memTable->insert(memTable, 1, 6, "Qwerty");
-    // memTable->insert(memTable, 1, 16, "Some beautiful data");
-    // memTable->insert(memTable, 1, 36, "1q2w3e4r");
+    memTable->insert(memTable, 1, 2, "Hello Table");
+    memTable->insert(memTable, 1, 4, "123");
+    memTable->insert(memTable, 1, 6, "Qwerty");
+    memTable->insert(memTable, 1, 16, "Some beautiful data");
+    memTable->insert(memTable, 1, 36, "1q2w3e4r");
 
-    // memTable->print(memTable);
+    memTable->print(memTable);
 
-    // memTable->delete (memTable, 1, 16);
-    // memTable->update(memTable, 1, 16, "Bla bla");
+    memTable->delete (memTable, 1, 16);
+    memTable->update(memTable, 1, 16, "Bla bla");
 
-    // memTable->print(memTable);
-
-    // memTable->find(memTable, 1, 36, (void **)&data);
-
-    // printf("%s\n", data);
-    printf("The end!\n");
+    memTable->print(memTable);
 
     return 0;
 }
